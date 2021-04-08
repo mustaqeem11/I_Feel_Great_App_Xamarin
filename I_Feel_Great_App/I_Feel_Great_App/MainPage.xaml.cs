4196b04f001bb7f3ed64b718545fe01d0d3bbf65
mustaqeem11 <mustaqeem11@hotmail.co.uk>
2021-04-08 22:29:53 +0100

    Add project files.

---
+++ I_Feel_Great_App/I_Feel_Great_App/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace I_Feel_Great_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async void Yes_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("System", "It is highly likely this is a female", "Ok");
+        }
+        async void No_OnClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("System", "It is likely this is male", "Ok");
+        }
+    }
+}
